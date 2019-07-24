0c6faddad793464a31abc966e4d9b3d75be3dbae
Brian <riceb53@gmail.com>
2019-07-24 18:38:00 -0500

    hide API key

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,5 +1,6 @@
 class Api::ArticlesController < ApplicationController
   def index
+    @articles = HTTP.get("https://newsapi.org/v2/everything?q=water&from=2019-06-24&sortBy=publishedAt&apiKey=#{ENV['api_key']}").parse
     render 'index.json.jb'
   end
 end
